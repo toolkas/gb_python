1c19db58c7a416b62c3a30e5a9d07cd6a0cb6c05
tulkas <d.rogov@id-mt.ru>
2020-02-14 12:50:31 +0300

    ДЗ 3: Задача 1

--- lesson3/task1.py
+++ lesson3/task1.py
@@ -1,2 +1,16 @@
 # 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+
+def divide_number(x, y):
+    return x / y
+
+
+first = float(input("Введите числитель: ").strip())
+second = float(input("Введите знаменатель: ").strip())
+
+try:
+    result = first / second
+    print(f"{first} / {second} = {result}")
+except ZeroDivisionError:
+    print("Введены некорректные данные: нельзя делить на ноль!")
